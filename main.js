f87aab23589c5f366854b375c2bd6401be4320b3
mayi <flying2010.liu@gmail.com>
2016-08-31 11:13:48 +0800

    init the project

---
+++ main.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './src/HelloWorld';
+
+ReactDOM.render(
+  <HelloWorld/>,
+  document.getElementById('root'));
---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+export default class HelloWolrd extends React.Component  {
+  
+  render() {
+    return (
+      <div>HelloWolrd</div>
+    )
+  }
+
+}
